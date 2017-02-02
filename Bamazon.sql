a57c9f6376cae6674be1268aa32046fda17910bc
Marcus Andersh <marcusandersh@Marcuss-MacBook-Pro.local>
2017-02-01 18:12:06 -0600

    done

---
+++ Bamazon.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Bamazon.products;
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ('Bike', 'sports', 200.00, 10)
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ('Nerf Gun', 'toys', 40.00, 40)
